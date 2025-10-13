487aada1f0f0fcaabf18fff420459993acd640d8
santosjuliana23 <a.pkskj@gmail.com>
2025-10-13 14:30:58 -0300

    refactor: campo de decoração refatorado #17

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,6 +2,6 @@ from .user import User, TelefoneUsuario
 from .salao import Salao, TelefoneSalao
 from .endereco import Endereco
 from .aluguel import Aluguel
-from .decoracao import Decoracao, FotoDecoracao
+# from .decoracao import Decoracao, FotoDecoracao
 from .servico import ServicoAdicional
 from .feedback import Feedback
--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,7 +2,7 @@ from .user import UserSerializer, UserCreateSerializer
 from .salao import SalaoSerializer
 from .endereco import EnderecoSerializer
 from .aluguel import AluguelSerializer
-from .decoracao import DecoracaoSerializer, FotoDecoracaoSerializer
+# from .decoracao import DecoracaoSerializer, FotoDecoracaoSerializer
 from .token import TokenSerializer
 from .servico import ServicoSerializer
 from .feedback import FeedbackSerializer
